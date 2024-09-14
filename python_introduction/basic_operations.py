8650d93a63d4118c8c30aa4b6e996651c355f7e3
abdulrahmanhilal520 <abdullrahmanhelal580@gmail.com>
2024-09-15 00:18:54 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,11 @@
 number1 = 10
 number2 = 5
-addition = number1 + number2 
-print("Addition = number1 + number2: "+ addition) 
-subtraction = number1 - number2 
-print("Subtraction = number1 - number2: " +subtraction)
+
+addition = number1 + number2
+print("Addition = number1 + number2: " + str(addition))
+
+subtraction = number1 - number2
+print("Subtraction = number1 - number2: " + str(subtraction))
+
 multiplication = number1 * number2
-print("Multiplication = number1 * number2: " + multiplication) 
+print("Multiplication = number1 * number2: " + str(multiplication))
