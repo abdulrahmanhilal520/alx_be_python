2211bd06f82a59d8d42bbdc641a0409ebbb5285d
Abdulrahman <abdullahmanhelal580@gmail.com>
2024-09-14 12:46:47 -0700

    py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+ddition = number1 + number2 
+print("Addition = number1 + number2: "+ addition) 
+subtraction = number1 - number2 
+print("Subtraction = number1 - number2: " +subtraction)
+multiplication = number1 * number2
+print("Multiplication = number1 * number2: " + multiplication) 
