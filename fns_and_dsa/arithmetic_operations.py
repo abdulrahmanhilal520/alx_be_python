b6194492a8bc086fee55e24679f39b5fc926bc48
abdulrahmanhilal520 <abdullrahmanhelal580@gmail.com>
2024-10-02 22:16:21 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,23 +1,22 @@
-
 def perform_operation(num1,num2,operation):
     
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ")
     
-        if operation = "add":
-            result = num1 + num2
-        elif operation = "subtract":
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
             result = num1 - num2
-        elif operation = "multiply":
+    elif operation == "multiply":
             result = num1 * num2
-        elif operation = "divide":
-            if num2 != 0:
+    elif operation == "divide":
+        if num2 != 0:
                 result = num1 / num2
-            else:
-                print("Error: Division by zero is not allowed")
-                return None
         else:
+            print("Error: Division by zero is not allowed")
+            return None
+    else:
             print("Invalid operation. Please choose one of (+, -, *, /).")
             return None
     return result
@@ -27,5 +26,5 @@ num2 = 3
 operation = 'add'
 result = perform_operation(num1, num2, operation)
 
-if __name__ == "__main__":
-    main()
+if result is not None:
+    print(f"result: {result}")
