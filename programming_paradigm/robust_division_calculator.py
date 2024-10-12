88c89bb8387df4fd4ab302fe6b3b5deddd1775e1
abdulrahmanhilal520 <abdullrahmanhelal580@gmail.com>
2024-10-12 10:35:44 +0300

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,1 +1,14 @@
-def safe_divide(numerator, denominator):     try:         num = float(numerator)         den = float(denominator)         result = num / den         return result      except ZeroDivisionError:         print("Division On Zero Not Allowed")         return None     except ValueError:         print("Invalid Input, pleas Enter Numbers Only ")         return None
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+        result = num / den
+        return result
+    
+    except ZeroDivisionError:
+        print("Division On Zero Not Allowed")
+        return None
+      
+    except ValueError:
+        print("Invalid Input, pleas Enter Numbers Only ")
+        return None
