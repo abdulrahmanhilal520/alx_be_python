8600642f1e1bee27ad8703738d385cf080cfa4c1
ABDULRAHMAN HILAL <abdullrahmanhelal580@gmail.com>
2024-10-12 10:34:33 +0300

    add Division clcoulate

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,1 @@
+def safe_divide(numerator, denominator):     try:         num = float(numerator)         den = float(denominator)         result = num / den         return result      except ZeroDivisionError:         print("Division On Zero Not Allowed")         return None     except ValueError:         print("Invalid Input, pleas Enter Numbers Only ")         return None
