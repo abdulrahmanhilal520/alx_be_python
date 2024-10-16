4a82c6838ddb68b3cc810b706ead1e4216c8bb00
abdulrahmanhilal520 <abdullrahmanhelal580@gmail.com>
2024-10-16 23:33:27 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,29 +3,41 @@ class Book:
         self.title = title
         self.author = author
 
-    def get_details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
+
+    def get_details(self):
+        return self.__str__()
 
 class EBook(Book):
     def __init__(self,title: str,author: str,file_size: int ):
         super().__init__(title,author)
         self.file_size = file_size
 
-    def get_details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+    def get_details(self):
+        return self.__str__()
 
 class PrintBook(Book):
     def __init__(self,title: str,author: str,page_count: int):
         super().__init__(title,author)
         self.page_count = page_count
 
-    def get_details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+    def get_details(self):
+        return self.__str__()
 
 class Library:
 
     def __init__(self):
         self.books = []
+
+    def __str__(self):
+        return f"Library with {len(self.books)} books"
 
     def add_book(self, book: Book):
         if not isinstance(book,Book):
@@ -38,4 +50,4 @@ class Library:
             print("Library is empty")
             return
         for book in self.books:
-            print(book.get_details())
+            print(book)
